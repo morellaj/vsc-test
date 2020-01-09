0eb5bc209eb792d80018b246383c9f9e2587b15d
morellaj <alexjamesmorelli@gmail.com>
2020-01-08 19:36:57 -0500

    more splitting

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,23 @@ module.exports = {
       '/api': 'http://localhost:8080',
     },
   },
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'all',
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+            return `npm.${packageName.replace('@', '')}`;
+          },
+        },
+      },
+    },
+  },
   module: {
     rules: [
       {
