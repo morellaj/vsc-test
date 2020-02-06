3d1f44f3f3ab6cf25ac92891830656b88c4beb8d
morellaj <alexjamesmorelli@gmail.com>
2020-02-05 23:21:03 -0500

    turned on ga debug

--- src/index.jsx
+++ src/index.jsx
@@ -11,7 +11,10 @@ import {
 import Error from 'Error';
 
 Sentry.init({ dsn: 'https://a247611c1b654f69aa4fed33d5789e5c@sentry.io/2274414' });
-ReactGA.initialize('UA-157541239-1');
+ReactGA.initialize('UA-157541239-1', {
+  debug: true,
+  titleCase: false,
+});
 ReactGA.pageview('/fun');
 
 // Component dependencies
