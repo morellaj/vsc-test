93de4ef22f7905091bc67944816d0466bb24dd54
morellaj <alexjamesmorelli@gmail.com>
2020-02-06 18:24:06 -0500

    removing service worker

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const { GenerateSW } = require('workbox-webpack-plugin');
+// const { GenerateSW } = require('workbox-webpack-plugin');
 // const SentryCliPlugin = require('@sentry/webpack-plugin');
 //  const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
@@ -69,7 +69,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
-    new GenerateSW(),
+    // new GenerateSW(),
     new CopyPlugin([
       { from: 'assets', to: 'assets' },
       { from: '_redirects', to: '_redirects', toType: 'file' },
