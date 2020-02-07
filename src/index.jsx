a9d77fdf2a42692b81bedd83dc6e52e93517448d
morellaj <alexjamesmorelli@gmail.com>
2020-02-06 22:04:03 -0500

    debugging refresh issue

--- src/index.jsx
+++ src/index.jsx
@@ -11,6 +11,7 @@ import {
 import Error from 'Error';
 // test
 // Component dependencies
+/*
 const HomePage = loadable(() => import('./components/home/HomePage'));
 const CharacterPage = loadable(() => import('./components/curriculum/CharacterPage'));
 const PDFPage = loadable(() => import('./components/pdfs/PDFPage'));
@@ -20,8 +21,9 @@ const FeedbackPage = loadable(() => import('./components/other/FeedbackPage'));
 const UpdateSignUpPage = loadable(() => import('./components/other/UpdateSignUpPage'));
 const TopicRecommendationPage = loadable(() => import('./components/other/TopicRecommendationPage'));
 const ContactPage = loadable(() => import('./components/other/ContactPage'));
+*/
 
-/*
+
 import HomePage from './components/home/HomePage';
 import CharacterPage from './components/curriculum/CharacterPage';
 import PDFPage from './components/pdfs/PDFPage';
@@ -31,7 +33,7 @@ import FeedbackPage from './components/other/FeedbackPage';
 import UpdateSignUpPage from './components/other/UpdateSignUpPage';
 import TopicRecommendationPage from './components/other/TopicRecommendationPage';
 import ContactPage from './components/other/ContactPage';
-*/
+
 
 Sentry.init({ dsn: 'https://a247611c1b654f69aa4fed33d5789e5c@sentry.io/2274414' });
 ReactGA.initialize('UA-157541239-1');
