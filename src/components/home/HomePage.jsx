9594bd76e57b86c4c5842e75ef14fa45c121191e
morellaj <alexjamesmorelli@gmail.com>
2020-02-19 11:23:51 -0500

    seo

--- src/components/home/HomePage.jsx
+++ src/components/home/HomePage.jsx
@@ -35,8 +35,8 @@ export default function Home() {
           description={description}
           type="website"
           image="https://learningisthesolution.com/assets/Home1.jpg"
-          height="1519"
-          width="619"
+          height="1280"
+          width="521"
           url="https://learningisthesolution.com/"
         />
       </Suspense>
